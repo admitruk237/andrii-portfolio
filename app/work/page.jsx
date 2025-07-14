348b0873a53e240a829b4c08979ed10d1a07150c
Andrii Dmtryk <admitruk237@gmail.com>
2025-07-14 15:12:49 +0200

    complete initial setup for Work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,115 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { BsArrowUpRight, BsGithub } from 'react-icons/bs';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import Link from 'next/link';
+import Image from 'next/image';
+/* 
+Landing Pages & Simple Websites
+E-commerce & Collaborative
+React Applications
+Next.js Applications
+*/
+
+const projects = [
+  {
+    num: '01',
+    categoty: 'Landing Pages & Simple Websites',
+    title: 'Finance',
+    discription:
+      'A website for financial services with a clean and professional UI.',
+    stack: [{ name: 'Html 5' }, { name: 'Css 3' }, { name: 'Javascritp' }],
+    image: '/assets/work/finace.png',
+    github: 'https://admitruk237.github.io/Finance-site/',
+    live: 'https://github.com/admitruk237/Finance-site',
+  },
+];
+
 const Work = () => {
-  return <div>work page</div>;
+  const [project, setProject] = useState(projects[0]);
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 lg:py-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row lg:gap-[30px]">
+          <div
+            className="w-full lg:w-[50%] lg:h-[460px] flex flex-col lg:justify-between order-2
+          lg:order-none"
+          >
+            <div className="flex flex-col gap-[30px] h-[50%]">
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              <h2
+                className="text-[42px] font-bold leading-none text-white group-hover:text-accent
+              transition-all duration-500 "
+              >
+                {project.categoty}
+              </h2>
+              <p className="text-white/60">{project.discription}</p>
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-accent">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ','}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="border border-white/20"></div>
+              <div className="flex items-center gap-4">
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger
+                        className="w-[70px] h-[70px] rounded-full bg-white/5 
+                    flex justify-center items-center group"
+                      >
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger
+                        className="w-[70px] h-[70px] rounded-full bg-white/5 
+                    flex justify-center items-center group"
+                      >
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full lg:w-[50%]">slider</div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Work;
