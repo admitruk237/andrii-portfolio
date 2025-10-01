f3c97391c80d941a45bdc81e46794a4b0d9b94cd
Andrii Dmtryk <admitruk237@gmail.com>
2025-10-01 13:59:39 +0200

    fix: page transiton bug, and add new cv

--- components/PageTransition/PageTransition.tsx
+++ components/PageTransition/PageTransition.tsx
@@ -2,7 +2,7 @@
 
 import { AnimatePresence, motion } from 'framer-motion';
 import { usePathname } from 'next/navigation';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 interface PageTransitionProps {
   children: ReactNode;
@@ -10,20 +10,34 @@ interface PageTransitionProps {
 
 const PageTransition = ({ children }: PageTransitionProps) => {
   const pathname = usePathname();
+  const [isTransitionVisible, setIsTransitionVisible] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsTransitionVisible(true);
+    const hideTimer = setTimeout(() => {
+      setIsTransitionVisible(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(hideTimer);
+    };
+  }, [pathname]);
 
   return (
     <AnimatePresence mode="wait">
-      <div key={pathname}>
+      {isTransitionVisible && (
         <motion.div
+          key={pathname}
           initial={{ opacity: 1 }}
           animate={{
             opacity: 0,
             transition: { delay: 1, duration: 0.4, ease: 'easeInOut' },
           }}
+          exit={{ opacity: 0 }}
           className="h-screen w-screen fixed bg-primary top-0 pointer-events-none z-50"
         />
-        {children}
-      </div>
+      )}
+      {children}
     </AnimatePresence>
   );
 };
